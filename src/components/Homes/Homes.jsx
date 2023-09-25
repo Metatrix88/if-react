28f9934a8c4fcc4afadf31fa064bcc069f64d220
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-09-25 23:34:30 +0300

    cleaned cod

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -6,7 +6,7 @@ import { ContainerCards } from '../ContainerCards';
 import { Card } from '../Card';
 import { ImgLink } from '../ImgLink';
 
-import {homes} from '../ImgLink/config';
+import { homes } from '../ImgLink/config';
 
 // styles
 import './Homes.scss';
@@ -17,80 +17,14 @@ export const Homes = () => {
       <h2 className="homes__title">Homes guests loves</h2>
       <ContainerCards>
         {homes.map((home) => (
-        <Card key = {home.id} className="homes__card">
-          <ImgLink {...home} className='homes__link'></ImgLink>
-          <h3 className="homes__subtitle">{home.city}, {home.country}</h3>
-        </Card>
+          <Card key={home.id} className="homes__card">
+            <ImgLink {...home} className="homes__link"></ImgLink>
+            <h3 className="homes__subtitle">
+              {home.city}, {home.country}
+            </h3>
+          </Card>
         ))}
-
-
-
-          {/*<a className="link apartments__link" href="#" target="_blank">*/}
-          {/*  <img*/}
-          {/*    className="images apartments__img"*/}
-          {/*    src="src/images/jpg/apartments/hotel-leopold.jpg"*/}
-          {/*    alt="Hotel Leopold"*/}
-          {/*  />*/}
-          {/*  Hotel Leopold*/}
-          {/*</a>*/}
-          {/*<h3 className="apartments__subtitle">Saint Petersburg, Russia</h3>*/}
-
-        {/*<li className="col-lg-3 col-md-6 col-sm-3 apartments__card">*/}
-        {/*  <a className="link apartments__link" href="#" target="_blank">*/}
-        {/*    <img*/}
-        {/*      className="images apartments__img"*/}
-        {/*      src="src/images/jpg/apartments/hotel-leopold.jpg"*/}
-        {/*      alt="Hotel Leopold"*/}
-        {/*    />*/}
-        {/*    Hotel Leopold*/}
-        {/*  </a>*/}
-        {/*  <h3 className="apartments__subtitle">Saint Petersburg, Russia</h3>*/}
-        {/*</li>*/}
-        {/*<li className="col-lg-3 col-md-6 col-sm-3 apartments__card">*/}
-        {/*  <a className="link apartments__link" href="#" target="_blank">*/}
-        {/*    <img*/}
-        {/*      className="images apartments__img"*/}
-        {/*      src="src/images/jpg/apartments/apartment-sunshine.jpg"*/}
-        {/*      alt="Apartment Sunshine"*/}
-        {/*    />*/}
-        {/*    Apartment Sunshine*/}
-        {/*  </a>*/}
-        {/*  <h3 className="apartments__subtitle">*/}
-        {/*    Santa Cruz de Tenerife, Spain*/}
-        {/*  </h3>*/}
-        {/*</li>*/}
-        {/*<li className="col-lg-3 col-md-6 col-sm-3 apartments__card _mobile-none">*/}
-        {/*  <a className="link apartments__link" href="#" target="_blank">*/}
-        {/*    <img*/}
-        {/*      className="images apartments__img"*/}
-        {/*      src="src/images/jpg/apartments/villa-kunerad.jpg"*/}
-        {/*      alt="Villa Kunerad"*/}
-        {/*    />*/}
-        {/*    Villa Kunerad*/}
-        {/*  </a>*/}
-        {/*  <h3 className="apartments__subtitle">Vysokie Tatry, Slowakia</h3>*/}
-        {/*</li>*/}
-        {/*<li className="col-lg-3 col-md-6 col-sm-3 apartments__card _mobile-none">*/}
-        {/*  <a className="link apartments__link" href="#" target="_blank">*/}
-        {/*    <img*/}
-        {/*      className="images apartments__img"*/}
-        {/*      src="src/images/jpg/apartments/hostel-friendship.jpg"*/}
-        {/*      alt="Hostel Friendship"*/}
-        {/*    />*/}
-        {/*    Hostel Friendship*/}
-        {/*  </a>*/}
-        {/*  <h3 className="apartments__subtitle">Berlin, Germany</h3>*/}
-        {/*</li>*/}
       </ContainerCards>
-      {/*<button*/}
-      {/*  className="button-next apartments__button-next"*/}
-      {/*  type="button"*/}
-      {/*  aria-label="Next apartaments"*/}
-      {/*>*/}
-      {/*  <svg className="button-next__icon">*/}
-      {/*    <use href="./src/images/svg/sprite.svg#arrow" />*/}
-      {/*  </svg>*/}
-      {/*</button>*/}
     </Container>
   );
 };
