67c3423a9337b17ba0c1b3399feb598abb5e4696
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-10-09 10:17:57 +0300

    created dir HomesClass

---
+++ src/components/HomesClass/Homes.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+// components
+import { Container } from '../Container';
+import { ContainerCards } from '../ContainerCards';
+import { Card } from '../Card';
+import { Link } from '../UI/Link';
+import { Image } from '../UI/Image';
+
+import { homes } from './config';
+
+// styles
+import './Homes.scss';
+
+export class Homes extends Component {
+  render() {
+    return (
+      <Container className="homes">
+        <h2 className="homes__title">Homes guests loves</h2>
+        <ContainerCards>
+          {homes.map((home) => (
+            <Card key={home.id} className="homes__card">
+              <Link
+                {...home}
+                className="homes__link"
+                variant="card"
+                target="_blank"
+              >
+                <Image {...home} className="homes__img" />
+                {home.name}
+                <h3 className="homes__subtitle">
+                  {home.city}, {home.country}
+                </h3>
+              </Link>
+            </Card>
+          ))}
+        </ContainerCards>
+      </Container>
+    );
+  }
+}
